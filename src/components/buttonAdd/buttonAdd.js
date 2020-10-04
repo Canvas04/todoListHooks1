4db97f0526e5d4abd0cb995264f9d5534534e2dc
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-04 15:53:33 +0300

    Начинаю добавлять проп-тайпсы

--- src/components/buttonAdd/buttonAdd.js
+++ src/components/buttonAdd/buttonAdd.js
@@ -1,8 +1,14 @@
 import React from "react";
+import propTypes from 'prop-types';
+import Buttons from "../timer/buttons";
+
 export default function ButtonAdd({addItem}) {
   return <> 
   <button type ='button' className='btn btn-info' onClick={addItem} >
     Add item
   </button>
   </>;
+}
+ButtonAdd.propTypes = {
+  addItem : propTypes.func.isRequired
 }
